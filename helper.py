0b6666489ad219b725ff54184ff83ee7a6eeca5e
charissa8606 <cytseng@ucdavis.edu>
2022-03-09 10:07:36 +0800

    add vgg16 option

--- helper.py
+++ helper.py
@@ -3,15 +3,11 @@ import os
 import numpy as np
 import config
 import pandas as pd
-from keras.models import load_model
 
 # getting the current path
 current_path = os.getcwd()
 
-# loading the trained model
-predictor_model = load_model('ResNet50.model')
-
-def predictor(img_path):
+def predictor(img_path, predictor_model):
     image = cv2.imread(img_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (224, 224))
--- main.py
+++ main.py
@@ -4,37 +4,49 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
+from keras.models import load_model
 
 sns.set_style('dark')
 sns.set()
 st.title('Adult Mosquito Classifier')
 
-option = st.selectbox(
+model_option = st.selectbox(
+    'Select fine-tuned model',
+    ('ResNet50', 'VGG16'))
+
+if model_option == 'ResNet50':
+    predictor_model = load_model('ResNet50.model')
+
+if model_option == 'VGG16':
+    predictor_model = load_model('VGG16.model')
+
+input_option = st.selectbox(
     "Image input method",
     ("Upload an image", "Camera"))
 
-if option == "Upload an image":
+if input_option == "Upload an image":
     uploaded_file = st.file_uploader("Upload Image")
 
-if option == "Camera":
+if input_option == "Camera":
     uploaded_file = st.camera_input("Take a picture")
+
 
 def save_uploaded_file(uploaded_file):
     try:
         with open(os.path.join('images', uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
         return 1
+
     except:
         return 0
 
 
 if uploaded_file is not None:
-
     if save_uploaded_file(uploaded_file):
         # display the image
         display_image = Image.open(uploaded_file)
         st.image(display_image)
-        prediction = predictor(os.path.join('images', uploaded_file.name))
+        prediction = predictor(os.path.join('images', uploaded_file.name), predictor_model)
         os.remove('images/'+uploaded_file.name)
         st.text('Predictions:')
         fig, ax = plt.subplots()
